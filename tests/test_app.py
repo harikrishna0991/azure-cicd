c2dbe7900e888b72efad48ee446892d419cbbb1f
harikrishna0991 <145971654+harikrishna0991@users.noreply.github.com>
2025-08-03 22:25:34 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
-# tests/test_app.py
+
+import sys
+import os
+import pytest
+
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app.main import app
 
 def test_home():
